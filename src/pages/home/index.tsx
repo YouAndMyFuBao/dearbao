aeac646b98f840e615767269577bfce966a0fd22
eunchokang <wkdb0425@gmail.com>
2024-02-19 19:44:46 +0900

    feat: 데일리 미션 페이지 생성

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import NavigationModal from "../components/navigation-modal/navigation-modal";
 
 const Home = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const router = useRouter();
 
   const openModal = () => {
     setModalOpen(true);
@@ -14,7 +16,9 @@ const Home = () => {
   return (
     <>
       <button onClick={openModal}>햄버거 메뉴 버튼</button>
-      <button>데일리 미션 버튼</button>
+      <button onClick={() => router.push("/daily-mission")}>
+        데일리 미션 버튼
+      </button>
       <p>홈 화면</p>
       <NavigationModal isOpen={isModalOpen} onClose={closeModal} />
     </>
