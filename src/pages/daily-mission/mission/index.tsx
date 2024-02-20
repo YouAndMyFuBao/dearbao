30759b9b8bc73b5c200932d5f9914863c36b539b
eunchokang <wkdb0425@gmail.com>
2024-02-20 18:36:43 +0900

    feat: 미션 입력 글자수 제한 및 편지보내기 버튼 활성화/비활성화 구현

--- src/pages/daily-mission/mission/index.tsx
+++ src/pages/daily-mission/mission/index.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 
 const Excution = () => {
   const router = useRouter();
+  const [missionText, setMissionText] = useState<string>("");
+
+  const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const textareaValue = e.target.value;
+    if (textareaValue.length > 400) {
+      return;
+    }
+
+    setMissionText(textareaValue);
+  };
 
   return (
     <div>
@@ -11,12 +22,17 @@ const Excution = () => {
           <div>데일리 미션 내용</div>
           <div>
             <div>
-              <textarea></textarea>
+              <textarea
+                value={missionText}
+                onChange={handleTextareaChange}
+              ></textarea>
               <p>From. 00000 이모</p>
             </div>
-            <p>0/400</p>
+            <p>{missionText.length}/400</p>
           </div>
-          <button onClick={() => router.push("/home")}>편지 보내기</button>
+          <button onClick={() => router.push("/home")} disabled={!missionText}>
+            편지 보내기
+          </button>
         </div>
       </main>
     </div>
