a31327a12dc7a60f17c4b7652f9e11d1f0171d5e
eunchokang <wkdb0425@gmail.com>
2024-02-25 17:21:24 +0900

    feat: 편지 유효성 검사 구현

--- src/pages/daily-mission/mission/index.tsx
+++ src/pages/daily-mission/mission/index.tsx
@@ -3,10 +3,14 @@ import { getDailyMission } from "@/pages/api/getDailyMission";
 import { postMission } from "@/pages/api/postMission";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { SlangList } from "./constant/slang-list";
 
 const Excution = () => {
   const router = useRouter();
   const [missionText, setMissionText] = useState<string>("");
+  const [isDailyMissionValid, setIsDailyMissionValid] = useState<boolean>(true);
+  const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(false);
+  const [textErrorMessage, setErrorMessage] = useState<string>("");
   const nicknameFromQuery = router.query.nickname;
 
   const { data: dailyMissionData } = useQuery({
@@ -15,16 +19,43 @@ const Excution = () => {
   });
 
   const dailyMissionContent = dailyMissionData?.data.content;
+  console.log("isButtonEndabled", isButtonEnabled);
 
+  // 정제된 데일리미션 텍스트를 useState변수에 담음
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const textareaValue = e.target.value;
-    if (textareaValue.length > 400) {
-      return;
-    }
+
+    textValidationFunction(textareaValue);
 
     setMissionText(textareaValue);
+    setIsButtonEnabled(true);
+    console.log("isButtonEndabled", isButtonEnabled);
   };
 
+  // 데일리 미션 텍스트 유효성 검사
+  const textValidationFunction = (text: string) => {
+    const isTextNone = text.length === 0 ? true : false;
+    const isTextOver = text.length >= 400 ? true : false;
+    const isLengthValid = !isTextNone && !isTextOver;
+    const isSlangContained = SlangList.some((slang) => text.includes(slang));
+
+    if (isLengthValid && !isSlangContained) {
+      setIsDailyMissionValid(true);
+    } else {
+      if (isTextNone) {
+        setErrorMessage("내용을 입력해주세요.");
+        setIsDailyMissionValid(false);
+      } else if (isTextOver) {
+        setErrorMessage("400자 이하로 입력해주세요.");
+        setIsDailyMissionValid(false);
+      } else if (isSlangContained) {
+        setErrorMessage("비속어는 사용할 수 없어요.");
+        setIsDailyMissionValid(false);
+      }
+    }
+  };
+
+  // 편지 보내기
   const handlePostMission = () => {
     postMission(missionText);
     window.alert("데일리 미션 제출 완료하였습니다");
@@ -44,12 +75,19 @@ const Excution = () => {
               <textarea
                 value={missionText}
                 onChange={handleTextareaChange}
+                style={{ borderColor: isDailyMissionValid ? "black" : "red" }}
               ></textarea>
               <p>From. {nicknameFromQuery} 이모</p>
             </div>
+            {!isDailyMissionValid && (
+              <p style={{ color: "red" }}>{textErrorMessage}</p>
+            )}
             <p>{missionText.length}/400</p>
           </div>
-          <button onClick={handlePostMission} disabled={!missionText}>
+          <button
+            onClick={handlePostMission}
+            disabled={!isButtonEnabled || !isDailyMissionValid}
+          >
             편지 보내기
           </button>
         </div>
